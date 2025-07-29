e79c4a15ee09969deeec95062340a90ab4397249
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2025-07-29 14:19:47 -0400

    Change docker-compose

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI_LOCAL!;
+const uri = process.env.MONGODB_URI!;
 const client = new MongoClient(uri);
 
 declare global {
